b99a3c6d701a65dde8de1cb2f26b2c874ab80046
danizaopao <daniel.oliveira.senai.404@gmail.com>
2024-08-02 10:37:55 -0300

    php váriaveis

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<?php
+//declaração das Váriaveis
+$nome='daniel ';
+$escola='SESi Cerquilho ';
+$CE=428;
+$curso='Desenvolvimento de Sistema ';
+$media= 9.5;
+$cidade='cerquilho';
+//parte que vai aparecer escrita no site
+echo 'Olá Bem vindo ao SESI-SP <br>';
+
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+
+echo "Ola $nome, seja bem vindo ao SESI $CE de $cidade, sua nota no curso de $curso é: $media"
+
+
+
+?>
